18ede56baa70abc26de6cffe96c42d7379b4edb0
Pedro Fernandes <pedrohasftb@gmail.com>
2025-10-27 10:19:17 -0300

    make entities resolver a list

--- src/EntitiesResolver.sol
+++ src/EntitiesResolver.sol
@@ -8,32 +8,39 @@ import {Attestation} from "eas-contracts/Common.sol";
 contract EntitiesResolver is UpgradableSchemaResolver {
     error InvalidAttester();
 
-    event AttesterChanged(address indexed attester);
+    event AttesterAdded(address indexed attester);
+    event AttesterRemoved(address indexed attester);
 
-    address public attester;
+    mapping(address => bool) public attesters;
 
     function initialize(
         IEAS eas,
         address _owner,
-        address _attester
+        address[] memory _attesters
     ) public initializer {
         UpgradableSchemaResolver.initialize(eas, _owner);
-        attester = _attester;
 
-        emit AttesterChanged(_attester);
+        for (uint256 i = 0; i < _attesters.length; i++) {
+            attesters[_attesters[i]] = true;
+            emit AttesterAdded(_attesters[i]);
+        }
     }
 
-    function setAttester(address _attester) public onlyOwner {
-        attester = _attester;
+    function addAttester(address _attester) public onlyOwner {
+        attesters[_attester] = true;
+        emit AttesterAdded(_attester);
+    }
 
-        emit AttesterChanged(_attester);
+    function removeAttester(address _attester) public onlyOwner {
+        attesters[_attester] = false;
+        emit AttesterRemoved(_attester);
     }
 
     function onAttest(
         Attestation calldata attestation,
         uint256 /*value*/
     ) internal override returns (bool) {
-        if (attestation.attester != attester) {
+        if (!attesters[attestation.attester]) {
             revert InvalidAttester();
         }
 
